6caea3af512221330455a5e38238c180c2daf6be
nico <nicolasnk16@gmail.com>
2022-05-18 16:36:35 -0300

    Se agrega el borrador de prendas

---
+++ src/main/java/Borrador.java
@@ -0,0 +1,38 @@
+import static java.util.Objects.requireNonNull;
+
+public class Borrador {
+  private TipoPrenda tipoPrenda;
+  private Material material; //no nulo
+  private Color colorPrincipal; //no nulo
+  private Color colorSecundario; //puede ser nulo
+  private Trama trama = Trama.LISA;
+
+  public Borrador(TipoPrenda tipoPrenda) {
+    requireNonNull(tipoPrenda, "El tipo de la prenda no puede ser nulo");
+    this.tipoPrenda = tipoPrenda;
+  }
+
+  public void especificarMaterial(Material material){
+    requireNonNull(material, "El material no puede ser nulo");
+    this.material = material;
+  }
+
+  public void especificarColorPrincipal(Color colorPrincipal){
+    requireNonNull(colorPrincipal, "El color principal de la prenda no puede ser nulo");
+    this.colorPrincipal = colorPrincipal;
+  }
+
+  public void especificarColorSecundario(Color colorSecundario){
+    this.colorSecundario = colorSecundario;
+  }
+
+  public void especificarTrama(Trama trama){
+    if(trama != null)
+      this.trama = trama;
+  }
+
+  public Prenda crearPrenda(){
+    return new Prenda(tipoPrenda, material, colorPrincipal, colorSecundario, trama);
+  }
+
+}
--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -3,23 +3,16 @@ import static java.util.Objects.requireNonNull;
 public class Prenda {
 
     private TipoPrenda tipoPrenda;
-    private Material material; //no nulo
-    private Color colorPrincipal; //no nulo
-    private Color colorSecundario; //puede ser nulo
+    private Material material;
+    private Color colorPrincipal;
+    private Color colorSecundario;
     private Trama trama;
 
     public Prenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal, Color colorSecundario, Trama trama){
-        this.validarPrenda(tipoPrenda, material, colorPrincipal);
         this.material = material;
         this.colorPrincipal = colorPrincipal;
         this.colorSecundario = colorSecundario;
         this.trama = trama;
-    }
-
-    private void validarPrenda(TipoPrenda tipoPrenda, Material material, Color colorPrincipal){
-        requireNonNull(tipoPrenda, "El tipo de la prenda no puede ser nulo");
-        requireNonNull(material, "El material no puede ser nulo");
-        requireNonNull(colorPrincipal, "El color principal no puede ser nulo");
     }
 
     public boolean tieneColorSecundario(){
