2818a1ce4c2213e3aec93f7beb826a7cfac87007
nico <nicolasnk16@gmail.com>
2022-06-08 20:33:13 -0300

    Se elimina la posibilidad de que los guardarropas conozcan a sus usuarios.

--- src/main/java/sugerenciadeatuendos/Guardarropa.java
+++ src/main/java/sugerenciadeatuendos/Guardarropa.java
@@ -8,13 +8,10 @@ import java.util.List;
 public class Guardarropa {
   private Criterio criterio;
   private List<Prenda> prendas;
-  List<Usuario> usuariosDelGuardarropa;
-  Boolean compartido;
 
   public Guardarropa(Criterio criterio, List<Prenda> prendas) {
     this.criterio = criterio;
     this.prendas = prendas;
-    this.compartido = false;
   }
 
   public Criterio getCriterio() {
@@ -23,21 +20,6 @@ public class Guardarropa {
 
   public List<Prenda> getPrendas() {
     return prendas;
-  }
-
-  public void agregarUsuario(Usuario usuario) {
-    this.usuariosDelGuardarropa.add(usuario);
-    if(this.usuariosDelGuardarropa.size()>1){
-      this.compartido = true;
-    }
-  }
-
-  public List<Usuario> getUsuariosDelGuardarropa() {
-    return usuariosDelGuardarropa;
-  }
-
-  public Boolean getCompartido() {
-    return compartido;
   }
 
   public void eliminarPrenda(Prenda prenda) {
